4fedfb1e4b82fe6c6c1dbbbe3a2b93071d299757
Solomon B. Braimah <150222143+SBedeB@users.noreply.github.com>
2024-01-22 19:49:46 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,49 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+// http://expressjs.com/en/starter/basic-routing.html or // Basic route for the homepage
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Middleware for parsing JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// Middleware for logging requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ipaddress: ${req.ip} - language: ${req.headers['accept-language']} - software: ${req.headers['user-agent']}`);
+  next();
 });
 
+// Route handler for /api/whoami
+app.get('/api/whoami', (req, res) => {
+  const ipAddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
 
+  res.json({
+    ipaddress: ipAddress,
+    language: language,
+    software: software,
+  });
+});
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// Listen for requests
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+module.exports = app;
